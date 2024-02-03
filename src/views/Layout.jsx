a56ba5f5323044e1c35bb76b214ebf4c13b34cf6
Jeremiah Fallin <jeremiah.fallin@gmail.com>
2024-02-03 00:00:15 -0800

    Update Layout.jsx

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 
 import './Layout.css';
 import { auth } from '../api/config.js';
-import { useAuth, SignInButton, SignOutButton } from '../api/useAuth.jsx';
 
 /**
  * TODO: The links defined in this file don't work!
@@ -19,14 +18,6 @@ export function Layout() {
 			<div className="Layout">
 				<header className="Layout-header">
 					<h1>Smart shopping list</h1>
-					{!!user ? (
-						<div>
-							<span>Signed in as {auth.currentUser.displayName}</span> (
-							<SignOutButton />)
-						</div>
-					) : (
-						<SignInButton />
-					)}
 				</header>
 				<main className="Layout-main">
 					<Outlet />
