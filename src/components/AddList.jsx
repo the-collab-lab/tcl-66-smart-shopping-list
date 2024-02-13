377314933da04d1cb088edd0c6204bca69e76a81
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2024-02-13 10:52:11 -0700

    create AddList.jsx Update handleSubmit
    create new component for AddList form.
    swap AddList component for form in Home.jsx
    update error message in catch block
    clearlist name after neList is created

---
+++ src/components/AddList.jsx
@@ -0,0 +1,49 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { createList } from '../api';
+import { useAuth } from '../api/useAuth.jsx';
+
+export default function AddList({ setListPath }) {
+	const [listName, setListName] = useState('');
+	const [message, setMessage] = useState('');
+	const navigate = useNavigate();
+	const { user } = useAuth();
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		try {
+			const newList = createList(user.uid, user.email, listName);
+			const listPath = user.uid + '/' + listName;
+			if (newList) {
+				setListName('');
+				setMessage(`Your list, ${listName}, was successfully created.`);
+				setListPath(listPath);
+				setTimeout(() => {
+					navigate('/list');
+				}, 2000);
+			} else {
+				setMessage(`Your list, ${listName} was not created. Please try again.`);
+			}
+		} catch (err) {
+			console.error(err);
+			setMessage(
+				`An error occurred while creating your list, ${listName}: ${err.message}.`,
+			);
+		}
+	};
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="newList">Create a new shopping list:</label>
+				<input
+					type="text"
+					id="newList"
+					placeholder="enter list name"
+					onChange={(e) => setListName(e.target.value)}
+				></input>
+				<button type="submit">Create List</button>
+			</form>
+			{message && <p>{message}</p>}
+		</>
+	);
+}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,57 +1,14 @@
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 import './Home.css';
 import { SingleList } from '../components/SingleList';
-import { createList } from '../api';
-import { useAuth } from '../api/useAuth.jsx';
+import AddList from '../components/AddList.jsx';
 
 export function Home({ data, setListPath }) {
-	const [listName, setListName] = useState('');
-	const [message, setMessage] = useState('');
-	const navigate = useNavigate();
-	const { user } = useAuth();
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		try {
-			const newList = createList(user.uid, user.email, listName);
-			const listPath = user.uid + '/' + listName;
-
-			if (newList) {
-				setMessage(`Your list, ${listName}, was created.`);
-				setListPath(listPath);
-				setTimeout(() => {
-					navigate('/list');
-				}, 2000);
-			} else {
-				setMessage(
-					`Your list, ${listName}, was not created, please try again.`,
-				);
-				return;
-			}
-		} catch (err) {
-			console.error(err);
-			setMessage(`Your list ${listName} was not created, please try again.`);
-		}
-		return;
-	};
-
 	return (
 		<div className="Home">
 			<p>
 				Hello from the home (<code>/</code>) page!
 			</p>
-			<form onSubmit={handleSubmit}>
-				<label htmlFor="newList">Create a new shopping list:</label>
-				<input
-					type="text"
-					id="newList"
-					placeholder="enter list name"
-					onChange={(e) => setListName(e.target.value)}
-				></input>
-				<button type="submit">Create List</button>
-			</form>
-			{message && <p>{message}</p>}
+			<AddList setListPath={setListPath} />
 			<ul>
 				{data.map((list) => (
 					<SingleList
