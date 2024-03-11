353c5a2a2a99677277d13c6e77299fc015ceda3e
Hannah Machado <hannahmachado@yahoo.ca>
2024-03-11 10:49:00 -0400

    adjusted previous code and updated with new code to allow for undoItem function to work, accessibility functionality included

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,4 +1,5 @@
-import { updateItem } from '../api';
+import { useState } from 'react';
+import { updateItem, undoItem } from '../api';
 import { subtractDates } from '../utils';
 import { Timestamp } from 'firebase/firestore';
 import { calculateEstimate } from '@the-collab-lab/shopping-list-utils';
@@ -8,11 +9,14 @@ export function ListItem({
 	name,
 	listPath,
 	id,
+	previousLastPurchased,
+	previousNextPurchased,
 	dateLastPurchased,
 	dateNextPurchased,
 	totalPurchases,
 	dateCreated,
 }) {
+	const [showUndoButton, setShowUndoButton] = useState(false);
 	const todaysDate = Timestamp.now();
 
 	// if dateLastPurchased is true subtract it from dateNextPurchased, else subtract dateCreated from dateNextPurchased to get the estimated number of days till next purchase
@@ -36,25 +40,81 @@ export function ListItem({
 	);
 
 	const handleChecked = async () => {
+		setShowUndoButton(true);
+		if (subtractDates(todaysDate, dateLastPurchased)) {
+			alert('Item is already checked');
+			return;
+		}
+
 		try {
-			await updateItem(listPath, id, todaysDate, nextPurchaseEstimate);
+			await updateItem(
+				listPath,
+				id,
+				todaysDate,
+				dateLastPurchased,
+				dateNextPurchased,
+				nextPurchaseEstimate,
+			);
 		} catch (err) {
 			console.error(err);
 		}
 	};
 
+	const handleUndo = async () => {
+		try {
+			await undoItem(
+				listPath,
+				id,
+				previousLastPurchased,
+				previousNextPurchased,
+			);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	const handleFocus = () => {
+		setShowUndoButton(subtractDates(todaysDate, dateLastPurchased));
+	};
+
+	const handleBlur = () => {
+		setShowUndoButton(false);
+	};
+
+	const handleMouseEnter = () => {
+		setShowUndoButton(subtractDates(todaysDate, dateLastPurchased));
+	};
+
+	const handleMouseLeave = () => {
+		setShowUndoButton(false);
+	};
+
 	return (
-		<li className="ListItem">
+		<li
+			className="ListItem"
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+		>
 			<label>
 				{name}
 				<input
 					type="checkbox"
-					id={`checkbox-${id}`} // Unique identifier
+					id={`checkbox-${id}`}
 					name={name}
 					onChange={handleChecked}
+					onFocus={handleFocus}
 					checked={subtractDates(todaysDate, dateLastPurchased)}
 				></input>
 			</label>
+			{showUndoButton && (
+				<button
+					className="toggle-undo"
+					onClick={handleUndo}
+					onBlur={handleBlur}
+				>
+					undo
+				</button>
+			)}
 		</li>
 	);
 }
