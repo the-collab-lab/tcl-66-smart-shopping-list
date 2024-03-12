1cb81caf6fb2366ef3962243706d3c3f1c0486d0
Hannah Machado <hannahmachado@yahoo.ca>
2024-03-12 15:24:55 -0400

    started sorting logic

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { getDifferenceBetweenDates } from '../utils';
 import { ListItem } from '../components/ListItem';
 
 export function List({ data, listPath, loading }) {
@@ -21,6 +22,63 @@ export function List({ data, listPath, loading }) {
 	const filteredData = data.filter((item) =>
 		item.name.toLowerCase().includes(search.toLowerCase()),
 	);
+
+	const todaysDate = new Date();
+
+	const sorted = filteredData.sort((a, b) => {
+		const date1 = Math.floor(
+			getDifferenceBetweenDates(a.dateNextPurchased.toDate(), todaysDate),
+		);
+		const date2 = Math.floor(
+			getDifferenceBetweenDates(b.dateNextPurchased.toDate(), todaysDate),
+		);
+
+		const item1 = a.name.toLowerCase();
+
+		const item2 = b.name.toLowerCase();
+
+		const daysSinceLastTransaction1 = Math.floor(
+			getDifferenceBetweenDates(
+				todaysDate,
+				a.dateLastPurchased
+					? a.dateLastPurchased.toDate()
+					: a.dateCreated.toDate(),
+			),
+		);
+
+		const daysSinceLastTransaction2 = Math.floor(
+			getDifferenceBetweenDates(
+				todaysDate,
+				b.dateLastPurchased
+					? b.dateLastPurchased.toDate()
+					: b.dateCreated.toDate(),
+			),
+		);
+
+		// sort by value of days since last purchased
+		if (daysSinceLastTransaction1 >= 60 && daysSinceLastTransaction2 < 60) {
+			return 1;
+		} else if (
+			daysSinceLastTransaction1 < 60 &&
+			daysSinceLastTransaction2 >= 60
+		) {
+			return -1;
+		}
+
+		// if dates are not equal sort by difference of dates
+		if (date1 !== date2) {
+			return date1 - date2;
+		}
+
+		// if dates are equal sort by character value
+		if (item1 < item2) {
+			return -1;
+		} else if (item1 > item2) {
+			return 1;
+		}
+
+		return 0;
+	});
 
 	return (
 		<>
@@ -45,7 +103,7 @@ export function List({ data, listPath, loading }) {
 						</button>
 					</form>
 					<ul>
-						{filteredData.map((item) => (
+						{sorted.map((item) => (
 							<ListItem
 								key={item.id}
 								name={item.name}
