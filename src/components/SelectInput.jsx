36d5be67f20d98f4e70341db319194a2d65555fb
Hannah Machado <hannahmachado@yahoo.ca>
2024-03-28 07:02:56 -0400

    added default value and removed selected from option

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -9,8 +9,9 @@ const SelectInput = ({ id, label }) => {
 			<select
 				id={id}
 				className="cursor-pointer appearance-none bg-white shadow hover:shadow-md text-gray-900 text-sm rounded-lg focus:tcl-blue focus:tcl-blue block p-2.5 h-[42px] w-[300px]"
+				defaultValue="disabled"
 			>
-				<option value="" disabled selected>
+				<option value="disabled" disabled>
 					Select an option
 				</option>
 				<option value="7">Soon</option>
