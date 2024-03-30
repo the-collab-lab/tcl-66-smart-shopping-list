bc90c32d320c1db2f15477e3ff9100478aae916b
eonflower <eonflower.dev@gmail.com>
2024-03-30 15:34:15 -0700

    updated checked styling to work properly

--- src/components/Urgency.jsx
+++ src/components/Urgency.jsx
@@ -4,8 +4,10 @@ const Urgency = ({ isChecked, urgency }) => {
 	const { text, bg } = colorPicker(urgency);
 
 	return (
-		<div className={`${bg} flex items-center rounded-md px-2 py-1`}>
-			<span className={`${text} text-xs`}>
+		<div
+			className={`${isChecked ? 'bg-gray-200' : bg} flex items-center rounded-md px-2 py-1`}
+		>
+			<span className={`${isChecked ? 'text-gray-800' : text} text-xs`}>
 				{!isChecked ? urgency : 'checked'}
 			</span>
 		</div>
