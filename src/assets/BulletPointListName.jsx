61150ded0aa7e43a7d72db3051e17746e103a223
ecampos <ecampos.jr54@gmail.com>
2024-04-02 14:16:03 -0500

    add blue color effect to NavigationBarListItem when viewing the item's web page

--- src/assets/BulletPointListName.jsx
+++ src/assets/BulletPointListName.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-const BulletPointListName = () => {
+const BulletPointListName = ({
+	name,
+	windowLocationListPath,
+	localStorageListName,
+}) => {
 	return (
 		<svg
 			width="8"
 			height="9"
 			viewBox="0 0 8 9"
 			xmlns="http://www.w3.org/2000/svg"
-			className="fill-[#D1D5DB] group-hover:fill-[#1A56DB]"
+			className={`${windowLocationListPath && localStorageListName === name ? 'fill-[#1A56DB]' : null} group-hover:fill-[#1A56DB]`}
 		>
 			<rect y="0.978516" width="8" height="8" rx="4" />
 		</svg>
--- src/components/NavigationBarSingleList.jsx
+++ src/components/NavigationBarSingleList.jsx
@@ -17,17 +17,35 @@ export function NavigationBarSingleList({
 		navigate('/list');
 	}
 
+	const windowLocationListPath =
+		window.location.pathname.substring(
+			window.location.pathname.lastIndexOf('/') + 1,
+		) === 'list';
+	const localStorageListName = localStorage
+		.getItem('tcl-shopping-list-path')
+		.substring(
+			localStorage.getItem('tcl-shopping-list-path').lastIndexOf('/') + 1,
+		);
+
 	return (
-		<li className="flex-grow justify-between px-6 h-12 bg-list rounded-lg shadow-sm mt-4 hover:bg-[#EBF5FF] hover:bg-opacity-85">
+		<li
+			className={`flex-grow justify-between px-6 h-12 bg-list rounded-lg shadow-sm mt-4 hover:bg-[#EBF5FF] hover:bg-opacity-85 ${windowLocationListPath && localStorageListName === name ? 'bg-[#EBF5FF] bg-opacity-85' : null}`}
+		>
 			{/* Using Link instead of button */}
 			<Link
 				to="/list"
 				onClick={() => handleClick()}
 				className="flex items-center w-full h-full gap-2.5 group"
 			>
-				<BulletPointListName />
+				<BulletPointListName
+					windowLocationListPath={windowLocationListPath}
+					localStorageListName={localStorageListName}
+					name={name}
+				/>
 				<p>{name}</p>
 			</Link>
 		</li>
 	);
 }
+
+// <... className={`flex items-center space-x-2 px-4 py-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 border-l-4 border-white ${window.location.pathname === '/' ? 'border-red-200' : ''}`}>
