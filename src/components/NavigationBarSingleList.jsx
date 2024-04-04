660c7b669bb464651e8c19a62ff946fa2b6ee685
ecampos <ecampos.jr54@gmail.com>
2024-04-04 00:31:27 -0500

    fix blue coloring to nav bar single list element that is in the list view

--- src/components/NavigationBarSingleList.jsx
+++ src/components/NavigationBarSingleList.jsx
@@ -22,22 +22,18 @@ export function NavigationBarSingleList({
 			window.location.pathname.lastIndexOf('/') + 1,
 		) === 'list';
 
-	const localStorageListName = () => {
-		try {
-			localStorage
-				.getItem('tcl-shopping-list-path')
-				.substring(
-					localStorage.getItem('tcl-shopping-list-path').lastIndexOf('/') + 1,
-				);
-		} catch (e) {
-			console.error('error', e);
-		}
-	};
+	const localStorageListName =
+		localStorage
+			?.getItem('tcl-shopping-list-path')
+			?.substring(
+				localStorage.getItem('tcl-shopping-list-path').lastIndexOf('/') + 1,
+			) || '';
 
 	return (
 		<li
-			className={`flex-grow justify-between px-6 h-12 bg-list rounded-lg shadow-sm mt-4 hover:bg-[#EBF5FF] hover:bg-opacity-85 ${windowLocationListPath && localStorageListName === name ? 'bg-[#EBF5FF] bg-opacity-85' : null}`}
+			className={`flex-grow justify-between px-6 h-12 bg-list rounded-lg shadow-sm mt-4 hover:bg-[#ebf5ff] hover:bg-opacity-85 ${windowLocationListPath && localStorageListName === name ? 'bg-[#ebf5ff] bg-opacity-85' : null}`}
 		>
+			{/*The above hex code (#ebf5ff) only worked in the ternary operator in lowercase format, not uppercase.*/}
 			{/* Using Link instead of button */}
 			<Link
 				to="/list"
@@ -54,5 +50,3 @@ export function NavigationBarSingleList({
 		</li>
 	);
 }
-
-// <... className={`flex items-center space-x-2 px-4 py-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 border-l-4 border-white ${window.location.pathname === '/' ? 'border-red-200' : ''}`}>
